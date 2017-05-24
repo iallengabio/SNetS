b3e83fa34b0479bfeb4d3d133be9b434c34e7196
Iallen Gabio <iallen16@gmail.com>
2017-05-24 13:44:25 -0300

    Allow SNetS to send simulation results to the firebase (realtime database).

---
+++ src/main/java/simulationControl/parsers/parsers/SimulationRequest.java
@@ -0,0 +1,64 @@
+package simulationControl.parsers.parsers;
+
+/**
+ * Created by Iallen on 23/05/2017.
+ */
+public class SimulationRequest {
+    private NetworkConfig networkConfig;
+    private TrafficConfig trafficConfig;
+    private SimulationConfig simulationConfig;
+    private String status;
+    private Result result = new Result();
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public NetworkConfig getNetworkConfig() {
+        return networkConfig;
+    }
+
+    public void setNetworkConfig(NetworkConfig networkConfig) {
+        this.networkConfig = networkConfig;
+    }
+
+    public TrafficConfig getTrafficConfig() {
+        return trafficConfig;
+    }
+
+    public void setTrafficConfig(TrafficConfig trafficConfig) {
+        this.trafficConfig = trafficConfig;
+    }
+
+    public SimulationConfig getSimulationConfig() {
+        return simulationConfig;
+    }
+
+    public void setSimulationConfig(SimulationConfig simulationConfig) {
+        this.simulationConfig = simulationConfig;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public static class Result{
+
+        public String blockingProbability;
+        public String bandwidthBlockingProbability;
+        public String externalFragmentation;
+        public String relativeFragmentation;
+        public String spectrumUtilization;
+        public String transceiversUtilization;
+        public String spectrumStatistics;
+
+    }
+}
