829e12aaa0d73df513a13c237f69dcae33855e43
Iallen Gábio de Sousa Santos <iallen16@gmail.com>
2017-06-11 16:25:02 -0300

    Small fix in result manager classes. Fix the presentation of bandwidth at blocking probability result and bandwidth blocking probability result. (#17)

--- src/main/java/simulationControl/resultManagers/BlockingProbResultManager.java
+++ src/main/java/simulationControl/resultManagers/BlockingProbResultManager.java
@@ -176,7 +176,7 @@ public class BlockingProbResultManager {
 			String aux = "blocking probability per bandwidth" + sep + loadPoint;
 			
 			for (Double bandwidth : Util.bandwidths) {
-				String aux2 = aux + sep + (bandwidth/1073741824.0) + "Gbps" + sep + "all" + sep + "all" + sep + " ";
+				String aux2 = aux + sep + (bandwidth/1000000000.0) + "Gbps" + sep + "all" + sep + "all" + sep + " ";
 				for (Integer rep : replications) {
 					aux2 = aux2 + sep + pbs.get(loadPoint).get(rep).getProbBlockBandwidth(bandwidth);
 				}
@@ -197,7 +197,7 @@ public class BlockingProbResultManager {
 			String aux = "blocking probability per pair and bandwidth" + sep + loadPoint;
 			
 			for (Double bandwidth : Util.bandwidths) {
-				String aux2 = aux + sep + (bandwidth/1073741824.0) + "Gbps";
+				String aux2 = aux + sep + (bandwidth/1000000000.0) + "Gbps";
 				for (Pair pair :  Util.pairs) {
 					String aux3 = aux2 + sep + pair.getSource().getName() + sep + pair.getDestination().getName() + sep + " ";
 					for(Integer rep :  replications){
