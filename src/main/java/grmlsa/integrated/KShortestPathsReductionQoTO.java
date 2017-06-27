b064060e4627ecf546c939fc6b325b2385bdd9e4
Alexandre <alexandrefontinele@yahoo.com.br>
2017-06-27 17:57:04 -0300

    Implementation of the KShortestPathsReductionQoTO and the ArtificialBeeColony algorithms

---
+++ src/main/java/grmlsa/integrated/KShortestPathsReductionQoTO.java
@@ -0,0 +1,273 @@
+package grmlsa.integrated;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeSet;
+
+import grmlsa.NewKShortestPaths;
+import grmlsa.Route;
+import grmlsa.modulation.Modulation;
+import grmlsa.modulation.ModulationSelectionAlgorithmInterface;
+import grmlsa.modulation.ModulationSelectionByQoT;
+import grmlsa.modulation.ModulationSelector;
+import grmlsa.spectrumAssignment.FirstFit;
+import grmlsa.spectrumAssignment.SpectrumAssignmentAlgorithmInterface;
+import network.Circuit;
+import network.ControlPlane;
+import network.Link;
+import simulationControl.Util;
+import util.IntersectionFreeSpectrum;
+
+/**
+ * Based on the article:
+ *  - An Efficient IA-RMLSA Algorithm for Transparent Elastic Optical Networks (2017)
+ *  
+ * This version does not pre-establish the circuit under analysis to verify the impact on other 
+ * circuits already active in the network
+ * 
+ * @author Alexandre
+ */
+public class KShortestPathsReductionQoTO implements IntegratedRMLSAAlgorithmInterface {
+
+	private NewKShortestPaths kShortestsPaths;
+    private ModulationSelectionAlgorithmInterface modulationSelection;
+    private SpectrumAssignmentAlgorithmInterface spectrumAssignment;
+    
+    // A single sigma value for all pairs
+    private double sigma;
+    
+    // A sigma value for each pair
+	private HashMap<String, Double> sigmaForAllPairs;
+    
+	@Override
+	public boolean rsa(Circuit circuit, ControlPlane cp) {
+		
+		if (kShortestsPaths == null){
+        	kShortestsPaths = new NewKShortestPaths(cp.getMesh(), 4);
+        }
+        if (modulationSelection == null){
+        	modulationSelection = new ModulationSelectionByQoT();
+        	modulationSelection.setAvaliableModulations(ModulationSelector.configureModulations(cp.getMesh()));
+        }
+        if(spectrumAssignment == null){
+			spectrumAssignment = new FirstFit();
+			sigmaFileReader();
+		}
+
+        List<Modulation> avaliableModulations = modulationSelection.getAvaliableModulations();
+        List<Route> candidateRoutes = kShortestsPaths.getRoutes(circuit.getSource(), circuit.getDestination());
+        Route chosenRoute = null;
+        Modulation chosenMod = null;
+        int chosenBand[] = {999999, 999999}; // Value never reached
+        
+        double chosenWorstDeltaSNR = 0.0;
+		
+		double sigmaPair = sigma;
+		if(sigmaForAllPairs != null){
+			String pair = circuit.getPair().getSource().getName() + "-" + circuit.getPair().getDestination().getName();
+			sigmaPair = sigmaForAllPairs.get(pair);
+		}
+		
+		// To avoid metrics error
+		Route checkRoute = null;
+		Modulation checkMod = null;
+		int checkBand[] = null;
+        
+		for (int r = 0; r < candidateRoutes.size(); r++) {
+			Route routeTemp = candidateRoutes.get(r);
+			circuit.setRoute(routeTemp);
+			
+	    	double highestLevel = 0.0;
+	    	Modulation firstModulation = null; // First modulation format option
+	    	Modulation secondModulation = null; // Second modulation format option
+	    	
+			for(int m = 0; m < avaliableModulations.size(); m++){
+				Modulation mod = avaliableModulations.get(m);
+				
+				int numberOfSlots = mod.requiredSlots(circuit.getRequiredBandwidth());
+				List<int[]> merge = IntersectionFreeSpectrum.merge(routeTemp);
+				int band[] = spectrumAssignment.policy(numberOfSlots, merge, circuit);
+				
+				if(band != null){
+					checkRoute = routeTemp;
+					checkMod = mod;
+					checkBand = band;
+					
+					boolean circuitQoT = cp.getMesh().getPhysicalLayer().isAdmissibleModultion(circuit, routeTemp, mod, band);
+					
+					if(circuitQoT){
+						double modulationSNRthreshold = mod.getSNRthreshold();
+						double circuitDeltaSNR = circuit.getSNR() - modulationSNRthreshold;
+						
+						if(circuitDeltaSNR >= sigmaPair){ // Tries to choose the modulation format that respects the sigma value
+							firstModulation = mod;
+						}
+						if(mod.getBitsPerSymbol() > highestLevel){ // Save the modulation format with the highest level as the second choice option
+							secondModulation = mod;
+							highestLevel = mod.getBitsPerSymbol();
+						}
+					}
+				}
+			}
+			
+			if((firstModulation == null) && (secondModulation != null)){ // Check the modulation format options
+				firstModulation = secondModulation;
+			}
+			
+			if(firstModulation != null){
+				int numberOfSlots = firstModulation.requiredSlots(circuit.getRequiredBandwidth());
+				List<int[]> merge = IntersectionFreeSpectrum.merge(routeTemp);
+				int band[] = spectrumAssignment.policy(numberOfSlots, merge, circuit);
+				
+				if(band != null){
+					List<Circuit> circuits = new ArrayList<Circuit>();
+					for (Link link : routeTemp.getLinkList()) {
+						TreeSet<Circuit> circuitsAux = link.getCircuitList();
+						
+						for(Circuit circuitTemp : circuitsAux){
+							if(!circuit.equals(circuitTemp) && !circuits.contains(circuitTemp)){
+								circuits.add(circuitTemp);
+							}
+						}
+					}
+					
+					double worstDeltaSNR = Double.MAX_VALUE;
+					for(int i = 0; i < circuits.size(); i++){
+						Circuit circuitTemp = circuits.get(i);
+						
+						double deltaSNR = cp.getDeltaSNR(circuitTemp);
+						if(deltaSNR < worstDeltaSNR){
+							worstDeltaSNR = deltaSNR;
+						}
+					}
+					
+					if((band[0] < chosenBand[0]) && (worstDeltaSNR >= chosenWorstDeltaSNR)){
+						chosenBand = band;
+						chosenRoute = routeTemp;
+						chosenMod = firstModulation;
+						chosenWorstDeltaSNR = worstDeltaSNR;
+					}
+				}
+			}
+		}
+		
+		if(chosenRoute != null){ // If there is no route chosen is because no available resource was found in any of the candidate routes
+			circuit.setRoute(chosenRoute);
+			circuit.setModulation(chosenMod);
+			circuit.setSpectrumAssigned(chosenBand);
+			return true;
+			
+		}else{
+			if(checkRoute == null){
+				checkRoute = candidateRoutes.get(0);
+				checkMod = avaliableModulations.get(0);
+			}
+			circuit.setRoute(checkRoute);
+			circuit.setModulation(checkMod);
+			circuit.setSpectrumAssigned(checkBand);
+			return false;
+		}
+		
+	}
+
+	public void sigmaFileReader(){
+		sigma = 0.0;
+		
+		String separador = System.getProperty("file.separator");
+		String sigmaPathArqConfiguration = Util.projectPath + separador + "sigma.txt";
+		String sigmaForAllPairspathArqConfiguration = Util.projectPath + separador + "sigmaForAllPairs.txt";
+		
+		if(!Paths.get(sigmaForAllPairspathArqConfiguration).toFile().exists()){
+			System.err.println("\tArquivo com o valor do sigma para os pares nao encontrado. A simulacao vai utilizar um valor de sigma para todos os pares.");
+			
+		}else{
+			
+			sigmaForAllPairs = new HashMap<String, Double>();
+			
+			try {
+				FileReader fr = new FileReader(sigmaForAllPairspathArqConfiguration);
+				BufferedReader in = new BufferedReader(fr);
+				
+				while(in.ready()){
+					String linha[] = in.readLine().split(";");
+					
+					String pair = linha[0];
+					double sigmaPair = Double.valueOf(linha[1]);
+					
+					sigmaForAllPairs.put(pair, sigmaPair);
+				}
+				
+				in.close();
+			    fr.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(!Paths.get(sigmaPathArqConfiguration).toFile().exists()){
+			System.err.println("\tArquivo com o valor do sigma nao encontrado. A simulacao vai utilizar um valor padrao.");
+			
+		}else{
+			
+			try {
+				FileReader fr = new FileReader(sigmaPathArqConfiguration);
+				BufferedReader in = new BufferedReader(fr);
+				
+				String linha = in.readLine();
+				sigma = Double.valueOf(linha);
+				
+				in.close();
+			    fr.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void createFileSigmaAllPairs(int numberOfNodes, String pathFile, double sigmaAllPairs[]) {
+		int cont = 0;
+		
+		try {
+			FileWriter fw = new FileWriter(pathFile);
+			BufferedWriter out = new BufferedWriter(fw);
+			
+			StringBuilder sbaux = new StringBuilder();
+			for(int i = 1; i <= numberOfNodes; i++){
+				StringBuilder sb = new StringBuilder();
+				
+				for(int j = 1; j <= numberOfNodes; j++){
+					if(i != j){
+						
+						sb.append(i + "-" + j + ";" +  String.valueOf(sigmaAllPairs[cont]) + "\n");
+						cont++;
+					}
+				}
+				out.append(sb.toString());
+				sbaux.append(sb.toString());
+			}
+			//System.out.println(sbaux.toString());
+			
+			out.close();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/util/tools/artificialBeeColony/ArtificialBeeColony.java
@@ -0,0 +1,298 @@
+package util.tools.artificialBeeColony;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * This class represents the Artificial Bee Colony (ABC) algorithm.
+ * 
+ * Code inspired by the java code for abc algorithm at artificial bee colony's website 
+ * found at http://mf.erciyes.edu.tr/abc/ and https://github.com/jimsquirt/JAVA-ABC.
+ *
+ * @author Alexandre
+ */
+public class ArtificialBeeColony {
+
+	// ABC PARAMETERS
+	private int maxLength; 		// The number of parameters of the problem to be optimized
+	private int NP; 			// The number of total bees/colony size. employed + onlookers
+    private int foodNumber; 	// The number of food sources equals the half of the colony size
+    private int trialLimit;  	// A food source which could not be improved through "limit" trials is abandoned by its employed bee
+    private int maxEpoch; 		// The number of cycles for foraging {a stopping criteria}
+    
+    private double minX;        // Minimum value for a parameter
+    private double maxX;        // Maximum value for a parameter
+    
+    private Random rand;
+    private ArrayList<FoodSource> foodSources;
+    private FoodSource gBest;
+    private int epoch;
+
+    /**
+     * Instantiates the artificial bee colony algorithm along with its parameters.
+     * 
+     * @param n int - numger of parameters (dimension) of the problem
+     * @param minX double - Minimum value for a dimension
+     * @param maxX double - Maximum value for a dimension
+     */
+    public ArtificialBeeColony(int n, double minX, double maxX) {
+    	this.maxLength = n;
+    	this.NP = 40;
+    	this.foodNumber = NP/2;
+    	this.trialLimit = 50;
+    	this.maxEpoch = 1000;
+    	this.gBest = null;
+    	this.epoch = 0;
+        
+        this.minX = minX;
+        this.maxX = maxX;
+    }
+
+    /**
+     * This method executes the ABC algorithm
+     * 
+     * @return boolean
+     */
+    public boolean execute() {
+    	foodSources = new ArrayList<FoodSource>();
+        rand = new Random();
+        epoch = 0;
+        boolean done = false;
+
+        initialize();
+        memorizeBestFoodSource();
+
+        while(!done) {
+            if(epoch < maxEpoch) {
+                if(gBest.isSatisfiedCondition()) {
+                    done = true;
+                }
+                sendEmployedBees();
+                calculateProbabilities();
+                sendOnlookerBees();
+                memorizeBestFoodSource();
+                sendScoutBees();
+                
+                epoch++;
+                
+                // This is here simply to show the runtime status.
+                System.out.println("Epoch: " + epoch);
+            } else {
+                done = true;
+            }
+        }
+        
+        System.out.println("done.");
+        System.out.println("Completed " + epoch + " epochs.");
+        
+        return done;
+    }
+
+    /**
+     * Sends the employed bees to optimize the solution
+     */
+    public void sendEmployedBees() {
+        int neighborBeeIndex = 0;
+        FoodSource currentBee = null;
+        FoodSource neighborBee = null;
+        
+        for(int i = 0; i < foodNumber; i++) {
+        	
+        	//randomly chosen food source different from the i-th
+        	neighborBeeIndex = getExclusiveRandomNumber(foodNumber - 1, i);
+            currentBee = foodSources.get(i);
+            neighborBee = foodSources.get(neighborBeeIndex);
+        	
+            sendToWork(currentBee, neighborBee);
+        }
+    }
+
+    /**
+     * Sends the onlooker bees to optimize the solution.
+     * Onlooker bees work on the best solutions from the employed bees.
+     * Best solutions have high selection probability.
+     */
+    public void sendOnlookerBees() {
+    	int i = 0;
+        int t = 0;
+        int neighborBeeIndex = 0;
+        FoodSource currentBee = null;
+        FoodSource neighborBee = null;
+
+        while(t < foodNumber) {
+            currentBee = foodSources.get(i);
+            
+            if(rand.nextDouble() < currentBee.getSelectionProbability()) {
+                t++;
+                
+                //randomly chosen food source different from the i-th
+                neighborBeeIndex = getExclusiveRandomNumber(foodNumber - 1, i);
+	            neighborBee = foodSources.get(neighborBeeIndex);
+	            
+	            sendToWork(currentBee, neighborBee);
+            }
+            
+            i++;
+            if(i == foodNumber) {
+                i = 0;
+            }
+        }
+    }
+
+    /**
+     * The optimization part of the algorithm.
+     * Improves the current bee by choosing a random neighbor bee. 
+     * The changes is a randomly generated number of times to try and improve the current solution.
+     * 
+     * @param currentBee FoodSource
+     * @param neighborBee FoodSource
+     */
+    public void sendToWork(FoodSource currentBee, FoodSource neighborBee) {
+    	double newValue = 0;
+    	
+        double currentFitness = currentBee.getFitness();
+
+        //The parameter (dimension) to be changed is determined randomly
+        int parameterToChange = getRandomNumber(0, maxLength - 1);
+        
+        //Produce a new candidate solution
+        double currentValue = currentBee.getNectar()[parameterToChange];
+        double neighborValue = neighborBee.getNectar()[parameterToChange];
+        double rid = (rand.nextDouble() - 0.5) * 2; //It is a random real number within the ranger [-1, 1]
+        newValue = currentValue + rid * (currentValue - neighborValue);
+        
+        //trap the value within upper bound and lower bound limits
+        if(newValue < minX){
+        	newValue = minX;
+        }
+        if(newValue > maxX){
+        	newValue = maxX;
+        }
+        
+        //Evaluate the new candidate solution
+        currentBee.getNectar()[parameterToChange] = newValue;
+        currentBee.toEvaluate();
+        double newFitness = currentBee.getFitness();
+        
+        //greedy selection
+        if(currentFitness <= newFitness) { //No improvement
+            currentBee.getNectar()[parameterToChange] = currentValue;
+            currentBee.setTrials(currentBee.getTrials() + 1);
+            currentBee.setFitness(currentFitness);
+            
+        } else { //improved solution
+            currentBee.setTrials(0);
+        }   
+        
+    }
+
+    /**
+     * Finds food sources which have been abandoned/reached the limit.
+     * Scout bees will generate a totally random solution from the existing and it will also reset its trials back to zero.
+     */
+    public void sendScoutBees() {
+    	FoodSource currentBee = null;
+        
+        for(int i = 0; i < foodNumber; i++) {
+            currentBee = foodSources.get(i);
+            
+            if(currentBee.getTrials() >= trialLimit) {
+            	currentBee.initializeNectar(rand, minX, maxX);
+                currentBee.setTrials(0);
+            }
+        }
+    }
+    
+    /**
+     * Sets the selection probability of each solution.
+     * The higher the fitness the greater the probability 
+     */
+	public void calculateProbabilities() {
+		FoodSource thisFood = null;
+        double sum = 0.0;
+        
+        for(int i = 1; i < foodNumber; i++) {
+            thisFood = foodSources.get(i);
+            
+            if(thisFood.getFitness() >= 0.0) {
+                sum += 1.0 / (1.0 + thisFood.getFitness());
+            	
+            } else {
+            	sum += 1.0 + Math.abs(thisFood.getFitness());
+            }
+        }
+        
+        for(int j = 0; j < foodNumber; j++) {
+            thisFood = foodSources.get(j);
+            thisFood.setSelectionProbability(thisFood.getFitness() / sum);
+        }
+    }
+
+	/**
+	 * Initializes food locations
+	 */
+    public void initialize() {
+        for(int i = 0; i < foodNumber; i++) {
+        	FoodSource newFoodSource = new FoodSource(maxLength, rand, minX, maxX);
+        	newFoodSource.toEvaluate();
+        	
+        	foodSources.add(newFoodSource);
+        }
+    }
+
+    /**
+     * Gets a random number in the range of the parameters
+     * 
+     * @param low int -  the minimum random number
+     * @param high int - the maximum random number
+     * @return int
+     */
+    public int getRandomNumber(int low, int high) {
+        return (int)Math.round((high - low) * rand.nextDouble() + low);
+    }
+
+    /**
+     * Gets a random number with the exception of the parameter
+     * 
+     * @param high int - the maximum random number
+     * @param except int - number to to be chosen
+     * @return int
+     */
+    public int getExclusiveRandomNumber(int high, int except) {
+        boolean done = false;
+        int getRand = 0;
+
+        while(!done) {
+            getRand = rand.nextInt(high);
+            if(getRand != except){
+                done = true;
+            }
+        }
+
+        return getRand;     
+    }
+    
+    /**
+     * Memorizes the best solution
+     */
+    public void memorizeBestFoodSource() {
+    	gBest = foodSources.get(0);
+    	
+    	for(int i = 1; i < foodNumber; i++) {
+    		if(gBest.getFitness() > foodSources.get(i).getFitness()){
+    			gBest = foodSources.get(i);
+    		}
+    	}
+    }
+
+	/**
+	 * Returns the best solution
+	 * 
+	 * @return the gBest
+	 */
+	public FoodSource getgBest() {
+		return gBest;
+	}
+
+    
+}
