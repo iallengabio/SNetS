28e8cb171b43451a4a66ee72ab080e8797fafdb9
Iallen Gábio de Sousa Santos <iallen16@gmail.com>
2017-08-22 14:18:38 -0300

    Feature new grooming stg multihop variations (#29)
    * refatoring STGMultihop
    
    * refatoring STGMultihop renamed to MGMVH
    
    * new Multihop Grooming Algorithms

--- src/main/java/grmlsa/GRMLSA.java
+++ src/main/java/grmlsa/GRMLSA.java
@@ -41,7 +41,8 @@ public class GRMLSA {
     // Optical traffic aggregation
     public static final String GROOMING_OPT_NOTRAFFICGROOMING = "notrafficgrooming";
     public static final String GROOMING_OPT_SIMPLETRAFFICGROOMING = "simpletrafficgrooming";
-    public static final String GROOMING_OPT_STGMULTIHOP = "stgmultihop";
+    public static final String GROOMING_OPT_MGMVH = "mgmvh";
+    public static final String GROOMING_OPT_MGMPH = "mgmph";
 
     // Routing
     public static final String ROUTING_DJK = "djk";
@@ -120,8 +121,10 @@ public class GRMLSA {
                 return new NoTrafficGrooming();
             case GROOMING_OPT_SIMPLETRAFFICGROOMING:
                 return new SimpleTrafficGrooming();
-            case GROOMING_OPT_STGMULTIHOP:
-                return new STGMultihop();
+            case GROOMING_OPT_MGMVH:
+                return new MGMVH();
+            case GROOMING_OPT_MGMPH:
+                return new MGMPH();
             default:
                 return null;
         }
