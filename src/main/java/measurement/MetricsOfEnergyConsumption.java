87611bf9b057a5030bbd8137f6fc08e8a234d4f6
Alexandre Cardoso Fontinele <alexandrefontinele@gmail.com>
2018-08-29 18:29:06 -0300

    Bugfix grooming (#42)
    * Metrics of the energy consumption
    Corrections in the way the general power consumption of the grid was
    being computed. Are now counted:
    - Average general power consumption;
    - Energy efficiency;
    - Total data transmitted;
    - Total energy consumption by transponders, OXCs and amplifiers.
    
    * Metrics of energy consumption
    Adding comments and improving method names
    
    * Metrics of energy consumption
    Addition of total energy consumption in metric and guard band accounting
    removed of the energy consumption.

--- src/main/java/measurement/MetricsOfEnergyConsumption.java
+++ src/main/java/measurement/MetricsOfEnergyConsumption.java
@@ -3,7 +3,6 @@ package measurement;
 import network.Circuit;
 import network.ControlPlane;
 import network.EnergyConsumption;
-import network.Mesh;
 import request.RequestForConnection;
 import simulationControl.resultManagers.MetricsOfEnergyConsumptionResultManager;
 
@@ -21,10 +20,11 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 	private double sumGeneralPc;
 	private double obsGeneralPc;
 	
-	private double sumDataTransmitted;    
-    private double tempoDeSimulacao;
-    private double energyOXCsAndAmps;
+	private double totalDataTransmitted;    
+    private double totalNetworkOperationTime;
     private double totalEnergyTransponders;
+    private double totalEnergyOXCs;
+    private double totalEnergyAmplifiers;
 
     /**
      * Creates a new instance of MetricsOfEnergyConsumption
@@ -38,10 +38,11 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 		sumGeneralPc = 0.0;
 		obsGeneralPc = 0.0;
 		
-		sumDataTransmitted = 0.0;
-		tempoDeSimulacao = 0.0;
-		energyOXCsAndAmps = 0.0;
+		totalDataTransmitted = 0.0;
+		totalNetworkOperationTime = 0.0;
 		totalEnergyTransponders = 0.0;
+		totalEnergyOXCs = 0.0;
+		totalEnergyAmplifiers = 0.0;
 		
 		fileName = "_EnergyConsumption.csv";
 		resultManager = new MetricsOfEnergyConsumptionResultManager();
@@ -55,21 +56,21 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 	 * @param request - RequestForConnection
 	 */
 	public void addNewObservation(ControlPlane cp, boolean success, RequestForConnection request) {
-		double tempoDeFinalizacao = request.getTimeOfFinalizeHours() * 3600.0; // Converting to seconds
-		if(tempoDeFinalizacao > tempoDeSimulacao){
-			tempoDeSimulacao = tempoDeFinalizacao; // Seconds
+		double finalizeTime = request.getTimeOfFinalizeHours() * 3600.0; // Converting to seconds
+		if(finalizeTime > totalNetworkOperationTime){
+			totalNetworkOperationTime = finalizeTime; // Seconds
 		}
 		
-		if(energyOXCsAndAmps == 0.0){
-			double PcOxcsAndAmps = computeTotalPowerConsumptionOXCsAndAmps(cp.getMesh(), cp);
-			energyOXCsAndAmps = PcOxcsAndAmps; // Watt
+		if(totalEnergyOXCs == 0.0){
+			totalEnergyOXCs = EnergyConsumption.computeOxcsPowerConsumption(cp.getMesh().getNodeList()); // Watt
+			totalEnergyAmplifiers = EnergyConsumption.computeLinksPowerConsumption(cp.getMesh().getLinkList(), cp); // Watt
 		}
 		
 		if(success){
 			Double duracao = request.getTimeOfFinalizeHours() - request.getTimeOfRequestHours();
 			duracao *= 3600.0; // Converting to seconds
 			
-			sumDataTransmitted += duracao * request.getRequiredBandwidth(); // Total data transmitted by the flow (bits)
+			totalDataTransmitted += duracao * request.getRequiredBandwidth(); // Total data transmitted by the flow (bits)
 			totalEnergyTransponders += computeEnergyConsumptionTransponders(request, duracao, cp); // Total energy consumed by the flow (Joule = Watt * second)
 		}
 		
@@ -113,27 +114,13 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 	}
 	
 	/**
-	 * Return the total power consumption of the OXCs and amplifiers
-	 * 
-	 * @param mesh Mesh
-	 * @param cp ControlPlane
-	 * @return double Watt
-	 */
-	public double computeTotalPowerConsumptionOXCsAndAmps(Mesh mesh, ControlPlane cp){
-		double PCoxcs = EnergyConsumption.computeOxcsPowerConsumption(mesh.getNodeList());
-		double PClinks = EnergyConsumption.computeLinksPowerConsumption(mesh.getLinkList(), cp);
-		double PcOxcsAndAmps = PCoxcs + PClinks;
-		return PcOxcsAndAmps;
-	}
-	
-	/**
 	 * Return the energy efficiency
 	 * 
 	 * @return double bits/Joule
 	 */
 	public double getEnergyEfficiencyGeneral(){
-		double totalEcOXCsAndAmps = energyOXCsAndAmps * tempoDeSimulacao; // Joule
-		double ef = sumDataTransmitted / (totalEnergyTransponders + totalEcOXCsAndAmps); // bits/Joule
+		double totalEcOXCsAndAmps = (totalEnergyOXCs + totalEnergyAmplifiers) * totalNetworkOperationTime; // Joule
+		double ef = totalDataTransmitted / (totalEnergyTransponders + totalEcOXCsAndAmps); // bits/Joule
 		return ef;
 	}
 	
@@ -143,7 +130,7 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 	 * @return double bits
 	 */
 	public double getTotalDataTransmitted(){
-		return sumDataTransmitted;
+		return totalDataTransmitted;
 	}
 	
 	/**
@@ -156,12 +143,29 @@ public class MetricsOfEnergyConsumption  extends Measurement {
 	}
 	
 	/**
-	 * Returns the energy consumption of the Oxcs and the amplifiers
+	 * Returns the energy consumption of the OXCs
 	 * 
 	 * @return double Joule
 	 */
-	public double getTotalEnergyConsumptionOxcsAndAmps(){
-		return (energyOXCsAndAmps * tempoDeSimulacao);
+	public double getTotalEnergyConsumptionOXCs(){
+		return (totalEnergyOXCs * totalNetworkOperationTime);
 	}
 	
+	/**
+	 * Returns the energy consumption of the amplifiers
+	 * 
+	 * @return double Joule
+	 */
+	public double getTotalEnergyConsumptionAmplifiers(){
+		return (totalEnergyAmplifiers * totalNetworkOperationTime);
+	}
+	
+	/**
+	 * Returns the energy consumption of the network
+	 * 
+	 * @return double Joule
+	 */
+	public double getTotalEnergyConsumption(){
+		return (totalEnergyTransponders + ((totalEnergyOXCs + totalEnergyAmplifiers) * totalNetworkOperationTime));
+	}
 }
