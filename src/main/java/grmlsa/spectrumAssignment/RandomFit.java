6bf06d4df316051a842e7fcdf7118ea1d6dd509d
Alexandre <alexandrefontinele@yahoo.com.br>
2018-08-30 06:34:38 -0300

    Spectrum assignment TBSA added.

---
+++ src/main/java/grmlsa/spectrumAssignment/RandomFit.java
@@ -0,0 +1,60 @@
+package grmlsa.spectrumAssignment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import network.Circuit;
+import util.IntersectionFreeSpectrum;
+
+/**
+ * This class represents the spectrum allocation technique called Random Fit.
+ * This technique chooses the randomly free spectrum band that accommodates the request.
+ *
+ * @author Alexandre
+ */
+public class RandomFit implements SpectrumAssignmentAlgorithmInterface {
+
+    @Override
+    public boolean assignSpectrum(int numberOfSlots, Circuit circuit) {
+        List<int[]> composition = IntersectionFreeSpectrum.merge(circuit.getRoute());
+
+        int chosen[] = policy(numberOfSlots, composition, circuit);
+
+        if (chosen == null) return false;
+
+        circuit.setSpectrumAssigned(chosen);
+
+        return true;
+    }
+
+    /**
+     * Applies the policy of allocation of spectrum RandomFit
+     * 
+     * @param numberOfSlots int
+     * @param freeSpectrumBands List<int[]>
+     * @param circuit Circuit
+     * @return int[]
+     */
+    @Override
+    public int[] policy(int numberOfSlots, List<int[]> freeSpectrumBands, Circuit circuit){
+    	int chosen[] = null;
+		ArrayList<int[]> bandList = new ArrayList<int[]>();
+		
+		for (int[] band : freeSpectrumBands) { //checks and guard the free bands that can establish the requisition
+			if(band[1] - band[0] + 1 >= numberOfSlots){
+				int faixaTemp[] = band.clone();
+				bandList.add(faixaTemp);
+			}
+		}
+		
+		if(bandList.size() > 0){ //if you have free bands, choose one randomly
+			Random rand = new Random();
+			int indexBand = rand.nextInt(bandList.size());
+			chosen = bandList.get(indexBand);
+			chosen[1] = chosen[0] + numberOfSlots - 1; //it is not necessary to allocate the entire band, only the number of slots necessary
+		}
+		
+		return chosen;
+	}
+}
