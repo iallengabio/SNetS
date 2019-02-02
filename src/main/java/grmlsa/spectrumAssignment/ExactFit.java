2df61984476deda1d4acea2297b813dc183898bb
alexandrefontinele <alexandrefontinele@gmail.com>
2019-02-02 13:52:13 -0200

    Modified the implementation of exact fit to conform to the reference. Modified the uses of exact fit in First Last Exact Fit.

--- src/main/java/grmlsa/spectrumAssignment/ExactFit.java
+++ src/main/java/grmlsa/spectrumAssignment/ExactFit.java
@@ -10,9 +10,12 @@ import util.IntersectionFreeSpectrum;
 /**
  * This class represents the spectrum allocation technique called Exact Fit.
  * This technique attempts to allocate a range of spectrum with exactly the same size as the number of slots required by the new request.
- * If the spectrum band is not found, the request is allocated following the worst fit policy.
+ * If the spectrum band is not found, the request is allocated following the first fit policy.
  *
- * @author Felipe
+ *Implementation based on:
+ * - Routing and Spectrum Allocation in Elastic Optical Networks: A Tutorial (2015)
+ *
+ * @author Alexandre
  */
 
 public class ExactFit implements SpectrumAssignmentAlgorithmInterface {
@@ -35,31 +38,26 @@ public class ExactFit implements SpectrumAssignmentAlgorithmInterface {
     	int chosen[] = null;
         
         for (int[] band : freeSpectrumBands) {
-            int sizeBand = band[1] - band[0] + 1;
-            if (chosen == null) {
-                if (sizeBand == numberOfSlots) {
-                    chosen = band.clone();
-                    chosen[1] = chosen[0] + numberOfSlots - 1;//It is not necessary to allocate the entire band, just the amount of slots required
-                }
+        	int sizeBand = band[1] - band[0] + 1;
+            if (sizeBand == numberOfSlots) {
+                chosen = band.clone();
+                chosen[1] = chosen[0] + numberOfSlots - 1; //It is not necessary to allocate the entire band, just the amount of slots required
+                break;
             }
         }
-
-        if (chosen == null) {// did not find any contiguous tracks and is still available
-            // now just look for the free range with size farthest from the amount of slots required
-
-            int greaterDifference = -1;
-            for (int[] band : freeSpectrumBands) {
-                int sizeBand = band[1] - band[0] + 1;
-                if (sizeBand >= numberOfSlots) {
-                    if (sizeBand - numberOfSlots > greaterDifference) { //Found a band with more slots quantity "different"
-                        chosen = band.clone();
-                        chosen[1] = chosen[0] + numberOfSlots - 1;//It is not necessary to allocate the entire band, just the amount of slots required
-                        greaterDifference = sizeBand - numberOfSlots;
-                    }
-                }
-            }
+        
+        if (chosen == null) { // did not find any contiguous tracks and is still available
+            // now just look for the free range and apply first fit policy
+        	
+	        for (int[] band : freeSpectrumBands) {
+	            if (band[1] - band[0] + 1 >= numberOfSlots) {
+	                chosen = band.clone();
+	                chosen[1] = chosen[0] + numberOfSlots - 1; //It is not necessary to allocate the entire band, just the amount of slots required
+	                break;
+	            }
+	        }
         }
-
+        
         return chosen;
     }
 
