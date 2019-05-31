e3a5359205349d6f0761f73d9e688f29a43acbef
Iallen Gábio de Sousa Santos <iallen16@gmail.com>
2019-05-31 17:45:55 -0300

    New algorithm iacfqoto (#72)
    * refactoring some algorithm names.
    
    * bugfix at MTGSR algorithm. The number of virtual hops is respected in all cases.

--- src/main/java/grmlsa/integrated/CompleteSharingSSTG.java
+++ src/main/java/grmlsa/integrated/CompleteSharingSSTG.java
@@ -5,9 +5,8 @@ import grmlsa.NewKShortestPaths;
 import grmlsa.Route;
 import grmlsa.modulation.Modulation;
 import grmlsa.modulation.ModulationSelectionAlgorithmInterface;
-import grmlsa.spectrumAssignment.FirstFit;
 import grmlsa.spectrumAssignment.FirstFitExpansiveness;
-import grmlsa.spectrumAssignment.SpectrumAssignmentAlgorithmInterface;
+import grmlsa.spectrumAssignment.FirstFitExpansiveness2;
 import network.Circuit;
 import network.ControlPlane;
 import util.IntersectionFreeSpectrum;
@@ -16,20 +15,18 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This class implements an RMLSA algotithm based on Complete Sharing algorithm considering the expansiveness of the circuits.
- *  
- * In the Complete Sharing the route and the frequency slots are selected in order to allocate a range of 
- * spectrum closer to the beginning of the optical spectrum.
+ * This class implements the mechanism SSTG over algorithm CompleteSharing.
+ *  * SSTG is presented at:
  * 
  * @author Iallen
  */
-public class CompleteSharingEx implements IntegratedRMLSAAlgorithmInterface {
+public class CompleteSharingSSTG implements IntegratedRMLSAAlgorithmInterface {
 
 	private int k = 3; //This algorithm uses 3 alternative paths
     private int sigmaExpansiveness=0;
     private KRoutingAlgorithmInterface kShortestsPaths;
     private ModulationSelectionAlgorithmInterface modulationSelection;
-    private FirstFitExpansiveness spectrumAssignment;
+    private FirstFitExpansiveness2 spectrumAssignment;
 
 
     @Override
@@ -37,7 +34,7 @@ public class CompleteSharingEx implements IntegratedRMLSAAlgorithmInterface {
         if (kShortestsPaths == null){
         	kShortestsPaths = new NewKShortestPaths(cp.getMesh(), k); //This algorithm uses 3 alternative paths
             modulationSelection = cp.getModulationSelection();
-            spectrumAssignment = new FirstFitExpansiveness();
+            spectrumAssignment = new FirstFitExpansiveness2();
             Map<String, String> uv = cp.getMesh().getOthersConfig().getVariables();
             this.sigmaExpansiveness = Integer.parseInt((String)uv.get("sigmaExpansiveness"));
         }
