c5e11ea7abaea70a7500c985f929bc1f8d20ddd5
Iallen Gábio <iallen16@gmail.com>
2019-06-13 17:15:13 -0300

    including the snrimpact in the MTGSR algorithm

--- src/main/java/grmlsa/trafficGrooming/MTGSR.java
+++ src/main/java/grmlsa/trafficGrooming/MTGSR.java
@@ -10,24 +10,26 @@ import java.util.Map;
  * The parameters alfa, beta, gama, delta and epsilon has to be seted at 'others' config file.
  */
 public class MTGSR extends MultihopGrooming {
-    private Double alfa;
-    private Double beta;
-    private Double gama;
-    private Double delta;
-    private Double epsilon;
+    private Double alfa = 0.0;
+    private Double beta = 0.0;
+    private Double gama = 0.0;
+    private Double delta = 0.0;
+    private Double epsilon = 0.0;
+    private Double fi = 0.0;
     @Override
     protected double costFunction(MultihopSolution sol, RequestForConnection rfc, ControlPlane cp) {
         if (this.alfa == null) {
             Map<String, String> uv = cp.getMesh().getOthersConfig().getVariables();
-            this.alfa = Double.parseDouble((String)uv.get("alfa"));
-            this.beta = Double.parseDouble((String)uv.get("beta"));
-            this.gama = Double.parseDouble((String)uv.get("gama"));
-            this.delta = Double.parseDouble((String)uv.get("delta"));
-            this.epsilon = Double.parseDouble((String)uv.get("epsilon"));
+            if(uv.get("alfa")!=null) this.alfa = Double.parseDouble((String)uv.get("alfa"));
+            if(uv.get("beta")!=null) this.beta = Double.parseDouble((String)uv.get("beta"));
+            if(uv.get("gama")!=null) this.gama = Double.parseDouble((String)uv.get("gama"));
+            if(uv.get("delta")!=null) this.delta = Double.parseDouble((String)uv.get("delta"));
+            if(uv.get("epsilon")!=null) this.epsilon = Double.parseDouble((String)uv.get("epsilon"));
+            if(uv.get("fi")!=null) this.fi = Double.parseDouble((String)uv.get("fi"));
         }
 
         MultihopSolutionStatistics mss = sol.statistics;
-        double res = this.alfa * mss.physicalHops + this.beta * mss.virtualHops + this.gama * mss.spectrumUtilization + this.delta * (double)mss.transceivers - this.epsilon * mss.meanSNR;
+        double res = this.alfa * mss.physicalHops + this.beta * mss.virtualHops + this.gama * mss.spectrumUtilization + this.delta * (double)mss.transceivers - this.epsilon * mss.meanSNR + this.fi * mss.SNRImpact;
         return res;
     }
 }
