bb787e7b68a58893d618f4acc49808e284aaa354
Iallen Gábio <iallen16@gmail.com>
2019-06-25 13:03:49 -0300

    SNetS API.

--- src/main/java/simulationControl/SNetSAPI.java
+++ src/main/java/simulationControl/SNetSAPI.java
@@ -2,18 +2,25 @@ package simulationControl;
 
 import simulationControl.parsers.SimulationRequest;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class SNetSAPI {
 
-    public SimulationRequest runSimulation(SimulationRequest sr){
-        return null;
+    public SimulationRequest runSimulation(SimulationRequest sr, SimulationManagement.SimulationProgressListener spl){
+        SimulationManagement sm = new SimulationManagement(sr);
+        sm.startSimulations(spl);
+        return sr;
     }
 
-    public SimulationRequest readSimulation(String path, String name){
-        return null;
+    public SimulationRequest readSimulation(String path, String name) throws FileNotFoundException {
+        SimulationFileManager sfm = new SimulationFileManager();
+        return sfm.readSimulation(path,name);
     }
 
-    public void writeSimulation(SimulationRequest sr,String path, String name){
-
+    public void writeSimulation(SimulationRequest sr,String path) throws IOException {
+        SimulationFileManager sfm = new SimulationFileManager();
+        sfm.writeSimulation(path,sr);
     }
 
 }
