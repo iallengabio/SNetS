f37224f7535433fbb3c48b4f3ca69a4e45ad4bf2
Iallen Gábio <iallen16@gmail.com>
2020-04-09 13:57:29 -0300

    agg processing.

--- processing of results/Python/AGG.py
+++ processing of results/Python/AGG.py
@@ -6,7 +6,7 @@ Created on Wed Apr  8 17:34:41 2020
 """
 
 import SNetSPy as sp
-import pandas as pd
+import SSTG.py as sstg
 
 sol = ['MPH', 'MS', 'MVH']
 loads = ['1092','1274','1456','1638','1820']
@@ -15,3 +15,9 @@ path1 = 'C:/Users/ialle/Google Drive/Doutorado/Simulacoes/AGG/First/AGG'
 path2 = 'C:/Users/ialle/Google Drive/Doutorado/Simulacoes/AGG/First/MTGSR'
 #sp.plotLines(path,ln,sc)
 sp.plotLines2(path1,path2,loads,loads,sol,'AGG','MTGSR',0.05)
+
+
+
+#SSTG tunning
+path = 'C:/Users/ialle/Google Drive/Doutorado/Simulacoes/AGG/SSTG_tunning/MPH/evaluations'
+dfs = sstg.extractDFSMod(path,sp.ABBP,sp.MBBP,0.05)
